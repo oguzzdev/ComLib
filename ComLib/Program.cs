7016093b9f1781dcbd796ee219c7da34c71d7e72
Oğuzhan <=>
2022-09-11 15:18:36 +0300

    Server.cs example.

--- ComLib/Program.cs
+++ ComLib/Program.cs
@@ -10,7 +10,46 @@ namespace ComLib
     {
         static void Main(string[] args)
         {
-            
+            Server server = new Server();
+            Console.ReadKey();
+        }
+    }
+
+    class Server : ServerBehavior
+    {
+        public Server()
+        {
+            Start();
+        }
+
+        protected override void Started()
+        {
+            Console.WriteLine("Server started.");
+        }
+
+        protected override void Stoped()
+        {
+            Console.WriteLine("Server stoped.");
+        }
+
+        protected override void Joined(Client client)
+        {
+            Console.WriteLine($"Client-{client.id} joined.");
+        }
+
+        protected override void Disconnected(Client client)
+        {
+            Console.WriteLine($"Client-{client.id} disconnected.");
+        }
+
+        protected override void Sent(byte[] data,Client client)
+        {
+            Console.WriteLine($"Server >> Client-{client.id} : \"{Encoding.Unicode.GetString(data)}\"");
+        }
+
+        protected override void Read(byte[] data,Client client)
+        {
+            Console.WriteLine($"Client-{client.id} > \"{Encoding.Unicode.GetString(data)}\"");
         }
     }
 }
